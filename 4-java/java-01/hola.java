4d104bd122e7342cbe851a4796b0d8179a0ffa07
Hector Gonzalez <hgonzalezh1200@alumno.ipn.mx>
2022-08-16 17:35:54 -0500

    Subir apuntes intro a java 16-08-2022

---
+++ 4-java/java-01/hola.java
@@ -0,0 +1,12 @@
+public class hola {
+// Esto es una clase del JDK Java
+	//String miVariable = "Hola mundo java";
+  public static void main (String arg []){
+
+    //Esta es la declaración del método main
+	//String miVariable = "Hola mundo java saludo a:";
+  String miNombre = "Victor Hugo";
+      System.out.println ("Hola saludos a: " + miNombre);
+    }
+
+}
