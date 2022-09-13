dc4af6fa094573db3060da658bd7f6f77560964d
Hector Gonzalez <hgonzalezh1200@alumno.ipn.mx>
2022-09-12 21:15:19 -0500

    Agregar apuntes SpringTools 12-09-2022

---
+++ 7-spring/hola/src/main/java/com/generation/hogh/HolaMundo.java
@@ -0,0 +1,17 @@
+package com.generation.hogh;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+
+public class HolaMundo {
+	
+	/*@GetMapping("/")	
+	public String hola() {
+		return  "index";
+	}*/
+}
