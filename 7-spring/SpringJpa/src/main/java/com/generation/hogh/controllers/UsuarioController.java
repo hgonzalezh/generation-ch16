43fc1a0f9bcb03a04fb13b7f0f203fe7738bea97
Hector Gonzalez <hgonzalezh1200@alumno.ipn.mx>
2022-09-15 13:34:31 -0500

    Agregar apuntes consumo de API en front, http put and delete 15-09-2022

--- 7-spring/SpringJpa/src/main/java/com/generation/hogh/controllers/UsuarioController.java
+++ 7-spring/SpringJpa/src/main/java/com/generation/hogh/controllers/UsuarioController.java
@@ -1,19 +1,18 @@
 package com.generation.hogh.controllers;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.generation.hogh.models.UsuarioModel;
 import com.generation.hogh.services.UsuarioService;
 
 @RestController
 @RequestMapping("/usuario")
+@CrossOrigin(origins = "*", methods = { RequestMethod.GET, RequestMethod.POST, RequestMethod.DELETE,
+        RequestMethod.PUT })
 
 public class UsuarioController {
 	@Autowired
@@ -27,6 +26,26 @@ public class UsuarioController {
 	@PostMapping()
 	public UsuarioModel guardarUsuario(@RequestBody UsuarioModel usuario){
         return this.usuarioService.guardarUsuario(usuario);
+    }
+
+	@GetMapping( path = "/{id}")
+	public Optional<UsuarioModel> obtenerUsuarioPorId(@PathVariable("id") Long id) {
+        return this.usuarioService.obtenerPorId(id);
+    }
+		
+	@GetMapping("/query")
+	public ArrayList<UsuarioModel> obtenerUsuarioPorPrioridad(@RequestParam("prioridad") Integer prioridad){
+        return this.usuarioService.obtenerPorPrioridad(prioridad);
+    }
+	
+	@DeleteMapping( path = "/{id}")
+	public String eliminarPorId(@PathVariable("id") Long id){
+        boolean ok = this.usuarioService.eliminarUsuario(id);
+        if (ok){
+            return "Se eliminó el usuario con id " + id;
+        }else{
+            return "No pudo eliminar el usuario con id" + id;
+        }
     }
 
 
